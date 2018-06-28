f43d026287f7a419ae75970009ba3abf5cfd6ff0
Shu Peng <shpeng@microsoft.com>
2018-06-28 12:17:34 -0700

    Adding switch to choose whether a flipped image should be added to the list.

--- CNTK/TransferLearning/TNC_CreatLableFile.py
+++ CNTK/TransferLearning/TNC_CreatLableFile.py
@@ -1,26 +1,50 @@
 import os
 import pandas as pd
 
+# Switches to control whether we want to list following flipped image on list
+ADD_HORIZONTALLY_FLIPPED_IMG = False
+ADD_VERTICALLY_FLIPPED_IMG = False
+ADD_BOTH_FLIPPED_IMG = False
+
 base_folder = os.path.dirname(os.path.abspath(__file__))
-datasets_dir = os.path.join(base_folder, "DataSets")
-source_path = os.path.join(datasets_dir, "ByLabelsExt")
-data_label_file = os.path.join(datasets_dir, "RawDataLabelsExt.csv")
+data_sets_dir = os.path.join(base_folder, "DataSets")
+source_path = os.path.join(data_sets_dir, "ByLabelsExt")
+data_label_file = os.path.join(data_sets_dir, "RawDataLabelsExt.csv")
 
 df = pd.DataFrame(columns=['FileName', 'Format', 'Folder', 'Category', 'Label'])
 index = 0
 for root, dirs, files in os.walk(source_path):
     for name in files:
-        print(os.path.join(root,name))
+        folder_name = name[(name.find(')') + 1):name.rfind('-')]
+        head, tail = os.path.split(root)
+
+        prefix = name[:3]
+        if prefix == '(H)':
+            # Horizontally flipped image
+            if ADD_HORIZONTALLY_FLIPPED_IMG:
+                df.loc[index] = [name[:-4], 'JPG', folder_name, tail, tail]
+                index += 1
+        elif prefix == '(V)':
+            # Vertically flipped image
+            if ADD_VERTICALLY_FLIPPED_IMG:
+                df.loc[index] = [name[:-4], 'JPG', folder_name, tail, tail]
+                index += 1
+        elif prefix == '(B)':
+            # Flipped both vertically and horizontally
+            if ADD_BOTH_FLIPPED_IMG:
+                df.loc[index] = [name[:-4], 'JPG', folder_name, tail, tail]
+                index += 1
+        else:
+            # This is original image
+            df.loc[index] = [name[:-4], 'JPG', folder_name, tail, tail]
+            index += 1
+        print(os.path.join(root, name))
         print("Name        = ", os.path.basename(name))
         print("File Name   = ", name[:-4])
-        folder_name = name[(name.find(')')+1):name.rfind('-')]
         print("Folder Name = ", folder_name)
-        head, tail = os.path.split(root)
         print("Label       = ", tail)
         print("==================================================")
         print("")
-        df.loc[index] = [name[:-4], 'JPG', folder_name, tail, tail]
-        index += 1
 
 print(df.shape)
 df.to_csv(data_label_file, index=False, encoding='utf-8-sig')
