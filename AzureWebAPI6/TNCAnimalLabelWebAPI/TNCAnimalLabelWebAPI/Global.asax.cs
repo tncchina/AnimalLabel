35e1f49bfc4fe9f2b1d34a6057143813b0beb9af
YushanCao <yushan_cao@hotmail.com>
2018-07-08 22:03:28 -0700

    Implemented Http Post method.

---
+++ AzureWebAPI6/TNCAnimalLabelWebAPI/TNCAnimalLabelWebAPI/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace TNCAnimalLabelWebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            string pathValue = Environment.GetEnvironmentVariable("PATH");
+            string domainBaseDir = AppDomain.CurrentDomain.BaseDirectory;
+            string cntkPath = domainBaseDir + @"bin\";
+            pathValue += ";" + cntkPath;
+            Environment.SetEnvironmentVariable("PATH", pathValue);
+        }
+    }
+}
